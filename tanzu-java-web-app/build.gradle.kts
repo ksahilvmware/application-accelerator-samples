e7ee3bffb736cc4396eb13baf2a8774922387307
ksahil <ksahil@vmware.com>
2024-02-01 16:26:31 +0530

    greatiing changes

--- tanzu-java-web-app/build.gradle.kts
+++ tanzu-java-web-app/build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
     // Spring
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 
     // Test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
